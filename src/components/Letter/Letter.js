03e8e27cbef6cc34ac5077d0f1db002790586272
Konrad Korzeniewski <k.korzeniewski94@gmail.com>
2021-07-22 00:27:14 +0200

    Create and style letters

---
+++ src/components/Letter/Letter.js
@@ -0,0 +1,57 @@
+import React from "react"
+
+import "./Letter.scss"
+
+const letters = [
+  "a",
+  "ą",
+  "b",
+  "c",
+  "ć",
+  "d",
+  "e",
+  "ę",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "ł",
+  "m",
+  "n",
+  "ń",
+  "o",
+  "ó",
+  "p",
+  "q",
+  "r",
+  "s",
+  "ś",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "ź",
+  "ż",
+]
+
+const Letter = ({ letterSelect }) => {
+  const lettersTab = letters.map((letter, index) => (
+    <button
+      key={`${index} + 1000`}
+      className="keyboard__item"
+      onClick={() => letterSelect(letter)}
+      disabled={false}
+    >
+      {letter.toUpperCase()}
+    </button>
+  ))
+  return <div className="keyboard__list">{lettersTab}</div>
+}
+
+export default Letter
