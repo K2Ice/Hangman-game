6f3a99f16b85a9d26b0145b932a6ad4ec19ffdf4
Konrad Korzeniewski <k.korzeniewski94@gmail.com>
2021-07-22 00:28:30 +0200

    Add data as a prop in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
 
+const words = ["słowo", "słowa", "słówka"]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={words} />
   </React.StrictMode>,
   document.getElementById("root")
 )
